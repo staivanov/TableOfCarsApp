ad1fdb32d1ef55192d21f4c2626464daea24e801
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2023-12-19 10:04:53 +0200

    Rendering for a table content was fixed.

--- src/app/Cars/cars-list/cars-list.component.ts
+++ src/app/Cars/cars-list/cars-list.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Car } from '../../models/Car';
 import { YesOrNoPipe } from '../../shared/yes-or-no.pipe';
 import { CarsService } from '../../shared/cars.service';
@@ -16,33 +16,35 @@ import { SharedModule } from '../../shared/shared.module';
   styleUrl: './cars-list.component.css'
 })
 
-export class CarsListComponent {
-
+export class CarsListComponent implements OnInit {
 
   cars: Car[];
-    
-
   filteredCars: Car[];
+  private _carsService: Car[];
 
   private _filterBy: string;
   isPriceHidden: boolean = true;
-
-
-  constructor(private carsService: CarsService){
-      this.cars = carsService.getCars();
-  }
 
 
   get filterBy(): string {
     return this._filterBy;
   }
 
-
   set filterBy(value: string) {
     this._filterBy = value;
     this.filteredCars = this.executeFilter(value);
   }
 
+
+  constructor(private carsService: CarsService) {
+    this._carsService = this.carsService.getCars();
+  }
+
+
+  ngOnInit(): void {
+    this.cars = this._carsService;
+    this.filteredCars = this.cars;
+  }
 
   showPrice() {
     this.isPriceHidden = !this.isPriceHidden;
@@ -51,7 +53,7 @@ export class CarsListComponent {
   executeFilter(filteredBy: string): Car[] {
     filteredBy = filteredBy.toLowerCase();
 
-    return this.cars.filter((car: Car) => 
+    return this.cars.filter((car: Car) =>
       car.brand.toLowerCase().includes(filteredBy)
     );
   }
